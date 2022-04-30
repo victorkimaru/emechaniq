cddc2736613ad7ced26d71c931a6f631eb05c18a
victorkimaru <vik3kim@gmail.com>
2022-04-30 23:10:19 +0300

    change title

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>MechaniQ</title>
+<title>eMechaniQ</title>
 <meta name="keywords" content="" />
 <meta name="description" content="" />
 

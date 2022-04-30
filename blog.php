349517eb1793d5430953d1d1ba105b4f52f282e8
victorkimaru <vik3kim@gmail.com>
2022-04-30 23:15:58 +0300

    Added meta to blog

--- blog.php
+++ blog.php
@@ -5,6 +5,9 @@
 <title>eMechaniQ Blog</title>
 <meta name="keywords" content="" />
 <meta name="description" content="" />
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <!--
 Template 2026 Hexa Bokeh
 http://www.tooplate.com/view/2026-hexa-bokeh

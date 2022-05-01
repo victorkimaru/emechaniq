2ec98a1a9c7dc7441d8b974d40e217ded68d7f74
victorkimaru <vik3kim@gmail.com>
2022-05-01 11:38:31 +0300

    Changed title

--- blog.php
+++ blog.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>eMechaniQ Blog</title>
+<title>Our Journey</title>
 <link rel="icon" type="image/x-icon" href="/images/logo.jpeg">
 <meta name="keywords" content="" />
 <meta name="description" content="" />
